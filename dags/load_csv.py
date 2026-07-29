4fcf057de5c591ebf9a1145eb97c7000bf54ba1b
Patricia <marie.lou.trips@gmail.com>
2026-07-29 22:32:00 +0200

    delete import

--- dags/load_csv.py
+++ dags/load_csv.py
@@ -6,8 +6,6 @@ import os
 CSV_FILE = "/opt/airflow/data/sales.csv"
 
 # MySQL connection
-import os
-
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "host.docker.internal")
